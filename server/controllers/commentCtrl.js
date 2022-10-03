3e6913314a530bcd55c510b13bf6546f610b0202
kwontaehoon <gju04195@gmail.com>
2022-10-03 13:46:28 +0900

    update

--- server/controllers/commentCtrl.js
+++ server/controllers/commentCtrl.js
@@ -15,7 +15,9 @@ const commentCtrl = {
         // 여기는 쿼리스트링으로 parans가 없을 때 실행
         
         const id = req.query.id;
+        console.log('id: ', id);
         const writer = req.cookies.key;
+        console.log('writer: ', writer);
         const {content} = req.body; // 구조분해할당
 
         switch(true){
